54f2fc7add40dda71fa784b815b7871f11905016
jannejjj <61980297+jannejjj@users.noreply.github.com>
2023-06-07 15:48:28 +0200

    capacity test updates

--- src/test/java/CapacityTestSimulation.java
+++ src/test/java/CapacityTestSimulation.java
@@ -4,29 +4,23 @@ import io.gatling.javaapi.http.*;
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
-import java.time.Duration;
-
 public class CapacityTestSimulation extends Simulation {
 
         // Protocol Definition
         HttpProtocolBuilder httpProtocol = HttpDsl.http.baseUrl("http://localhost:8080/engine-rest")
-                        .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-                        .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
+                        .acceptHeader("application/json")
+                        .userAgentHeader("Gatling/Performance Test");
 
-        // Starting the process before the decision activity in order to test the
-        // capacity of the decision matrix
+        // Starting the process
         ScenarioBuilder scn = CoreDsl.scenario("Capacity Test")
                         .exec(http("Start process")
                                         .post("/process-definition/key/Process_0umpg4c/start")
                                         .body(RawFileBody("startRequestBody.json"))
                                         .check(status().is(200)));
-        // .exec(http("Check current activity")
-        // .get("/task")
-        // .check(status().is(200)));
 
         // Simulation
         public CapacityTestSimulation() {
-                this.setUp(scn.injectOpen(atOnceUsers(10)))
+                this.setUp(scn.injectOpen(atOnceUsers(1)))
                                 .protocols(httpProtocol);
         }
 
